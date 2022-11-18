9e34630070bd944f652e4b20892296fb8bc89895
bvquoc <bvquoc.dev@gmail.com>
2022-11-19 00:28:22 +0700

    operation (AC)

---
+++ bedao-mini14/operation.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+
+const int N = 100005;
+int n, q, a[N];
+
+signed main(void) {
+    ios::sync_with_stdio(0); cin.tie(nullptr);
+    #ifdef ziwok
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    
+    int n, q, x;
+    int mi = INT_MAX, mx = INT_MIN;
+    cin >> n >> q;
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        mi = min(mi, x);
+        mx = max(mx, x);
+    }
+
+    while (q--) {
+        cin >> x;
+        cout << ((mi <= x && x <= mx) ? "Yes\n" : "No\n");
+    }
+    return 0;
+}
