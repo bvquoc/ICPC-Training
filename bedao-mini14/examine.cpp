b86bc78b3c0730218eab64baf07aaaa70e09704e
bvquoc <bvquoc.dev@gmail.com>
2022-11-19 01:55:30 +0700

    examine (AC): remove Mul stupid function

--- bedao-mini14/examine.cpp
+++ bedao-mini14/examine.cpp
@@ -1,16 +1,8 @@
 #include <bits/stdc++.h>
 #define endl '\n'
-#define int long long
 using namespace std;
 
-using ii = pair<int,int>;
-long long Mul(long long x, long long y,long long m) {
-    x %= m; y %= m;
-    long long q = (long double) x * y / m;
-    long long r = (x * y - q * m) % m;
-    if (r < 0) r += m;
-    return r;
-}
+const int MOD = 1000000007;
 
 signed main(void) {
     ios::sync_with_stdio(0); cin.tie(nullptr);
@@ -21,20 +13,20 @@ signed main(void) {
     
     int nTest;
     cin >> nTest;
-    const int MOD = 1000000007;
-    long long n, res;
 
     
     while (nTest--) {
+        long long n;
         cin >> n;
-        int sum = n + 1;
+        long long sum = n + 1;
         for (int x = 2; 1LL * x * x <= n; ++x) {
             if (n % x != 0) continue;
-            int y = n / x;
-            if (__gcd(x, y) != 1) continue;
+            long long y = n / x;
+            if (__gcd(1LL * x, y) != 1) continue;
             sum += (x + y);
         }
-        res = Mul(n, sum, MOD);
+        n %= MOD; sum %= MOD;
+        long long res = (n * sum) % MOD;
         cout << res << endl;
     }
 
